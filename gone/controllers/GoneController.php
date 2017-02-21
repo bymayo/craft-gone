861be04601428197fee803f0c7bd89ed595e363f
Jason Mayo <jason@bymayo.co.uk>
2017-02-21 16:24:06 +0000

    Ability to remove gone entries from the CMS

---
+++ gone/controllers/GoneController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Gone
+ *
+ * @author    Jason Mayo
+ * @twitter   @madebymayo
+ * @package   Gone
+ *
+ */
+
+namespace Craft;
+
+class GoneController extends BaseController
+{
+	
+	protected $allowAnonymous = true;
+	
+	public function actionRemove()
+	{
+		
+        $id = craft()->request->getParam('id');
+		craft()->gone->remove($id);
+		return $this->redirect(craft()->request->getUrlReferrer());
+		
+	}
+	
+}
